e5ac4eb93924390431d25d5018c5d8aab3f88042
PizarroSofia <65994727+PizarroSofia@users.noreply.github.com>
2021-06-29 13:38:33 -0500

    utlimaAc

--- Dulcefina/Controllers/CarritoController.cs
+++ Dulcefina/Controllers/CarritoController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dulcefina.Models;
+using Dulcefina.Models.Interface;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +12,15 @@ namespace Dulcefina.Controllers
 {
     public class CarritoController : Controller
     {
+
+        //inyectar las dependencias
+        public readonly IUsuarioRepository _usuarioRepository;
+        public CarritoController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+        //fin de dependencias
+
         public IActionResult Index()
         {
             var Objsesion = HttpContext.Session.GetString("scliente");
@@ -22,5 +34,19 @@ namespace Dulcefina.Controllers
             }
 
         }
+
+
+        public List<DetallePedido> listaCarrito =new List<DetallePedido>();
+        
+        public void agregarCarrito()
+        {
+
+
+
+        }
+       
+        
+
+    
     }
 }
